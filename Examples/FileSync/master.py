1d7773c4c246ad45ad06ee6905a6384acbb8b7c6
Jacoby Thwaites <jacoby@sparkl.com>
2018-09-18 10:15:35 +0100

    Fixed for pycodestyle and pylint

--- Examples/FileSync/master.py
+++ Examples/FileSync/master.py
@@ -16,87 +16,95 @@ limitations under the License.
 Simple implementation of FileSync/Master service.
 """
 import os
-import sparkl_cli as sparkl
+from base64 import b64encode
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from base64 import b64encode
 
 
 class CustomHandler(FileSystemEventHandler):
-  """
-  Watchdog handler callbacks marshal arguments and
-  invoke the functions corresponding to the notify
-  operations on the master service.
-  """
-  def __init__(self, service):
-    self.service = service
+    """
+    Watchdog handler callbacks marshal arguments and
+    invoke the functions corresponding to the notify
+    operations on the master service.
+    """
+    def __init__(self, service):
+        self.service = service
 
-  def on_created(self, event):
-    binary = bytearray()
-    if not event.is_directory:
-      with open(event.src_path, mode='rb') as file:
-        binary = file.read()
+    def on_created(self, event):
+        binary = bytearray()
+        if not event.is_directory:
+            with open(event.src_path, mode='rb') as file:
+                binary = file.read()
 
-    put(self.service, event.is_directory, event.src_path, binary)
+        put(self.service, event.is_directory, event.src_path, binary)
 
-  def on_deleted(self, event):
-      delete(self.service, event.is_directory, event.src_path)
+    def on_deleted(self, event):
+        delete(self.service, event.is_directory, event.src_path)
 
-  def on_modified(self, event):
-    binary = bytearray()
-    if not event.is_directory:
-      with open(event.src_path, mode='rb') as file:
-        binary = file.read()
-      put(self.service, event.is_directory, event.src_path, binary)
+    def on_modified(self, event):
+        binary = bytearray()
+        if not event.is_directory:
+            with open(event.src_path, mode='rb') as file:
+                binary = file.read()
+            put(self.service, event.is_directory, event.src_path, binary)
 
-  def on_moved(self, event):
-    move(self.service, event.is_directory, event.src_path, event.dest_path)
+    def on_moved(self, event):
+        move(self.service, event.is_directory, event.src_path, event.dest_path)
 
 
 def onopen(service):
-  """
-  SPARKL callback initialises the observer with an instance
-  of the custom watchdog handler class.
-  """
-  event_handler = CustomHandler(service)
-  service.observer = Observer()
-  service.observer.schedule(event_handler, '.', recursive=True)
-  service.observer.start()
-  print("Master watchdog started in {cwd}".format(
-    cwd=os.getcwd()))
+    """
+    SPARKL callback initialises the observer with an instance
+    of the custom watchdog handler class.
+    """
+    event_handler = CustomHandler(service)
+    service.observer = Observer()
+    service.observer.schedule(event_handler, '.', recursive=True)
+    service.observer.start()
+    print("Master watchdog started in {cwd}".format(
+        cwd=os.getcwd()))
 
 
 def onclose(service):
-  """
-  SPARKL callback waits for the observer to stop before exiting.
-  """
-  service.observer.stop()
-  service.observer.join()
-  print("Master watchdog stopped")
+    """
+    SPARKL callback waits for the observer to stop before exiting.
+    """
+    service.observer.stop()
+    service.observer.join()
+    print("Master watchdog stopped")
 
 
 def put(service, is_dir, path, binary):
-  base64 = b64encode(binary).decode('utf-8')
-  service.notify({
-    'notify': 'Mix/Master/Put',
-    'data': {
-      'is_dir': is_dir,
-      'path':   path,
-      'bytes':  base64}})
+    """
+    Watchdog create event is notified on the mix.
+    """
+    base64 = b64encode(binary).decode('utf-8')
+    service.notify({
+        'notify': 'Mix/Master/Put',
+        'data': {
+            'is_dir': is_dir,
+            'path':   path,
+            'bytes':  base64}})
 
 
 def delete(service, is_dir, path):
-  service.notify({
-    'notify': 'Mix/Master/Delete',
-    'data': {
-      'is_dir': is_dir,
-      'path': path}})
+    """
+    Watchdog delete event is notified on the mix.
+    """
+    service.notify({
+        'notify': 'Mix/Master/Delete',
+        'data': {
+            'is_dir': is_dir,
+            'path': path}})
 
 
 def move(service, is_dir, old, path):
-  service.notify({
-    'notify': 'Mix/Master/Move',
-    'data': {
-      'is_dir': is_dir,
-      'old': old,
-      'path': path}})
+    """
+    Watchdog move event is notified on the mix.
+    """
+    service.notify({
+        'notify': 'Mix/Master/Move',
+        'data': {
+            'is_dir': is_dir,
+            'old': old,
+            'path': path}})
--- Examples/FileSync/slave.py
+++ Examples/FileSync/slave.py
@@ -20,66 +20,67 @@ from base64 import b64decode
 
 
 def put(event):
-  """
-  (Over)writes a directory or file.
-  """
-  is_dir = event['data']['is_dir']
-  path = event['data']['path']
+    """
+    (Over)writes a directory or file.
+    """
+    is_dir = event['data']['is_dir']
+    path = event['data']['path']
 
-  if is_dir:
-    os.makedirs(path, exist_ok=True)
-  else:
-    binary = b64decode(event['data']['bytes'])
+    if is_dir:
+        os.makedirs(path, exist_ok=True)
+    else:
+        binary = b64decode(event['data']['bytes'])
+
     with open(path, 'wb') as file:
         file.write(binary)
 
 
 def delete(event):
-  """
-  Deletes a directory or file. If the slave is not in sync,
-  this may cause an exception which is caught.
-  """
-  is_dir = event['data']['is_dir']
-  path = event['data']['path']
+    """
+    Deletes a directory or file. If the slave is not in sync,
+    this may cause an exception which is caught.
+    """
+    is_dir = event['data']['is_dir']
+    path = event['data']['path']
 
-  try:
-    if is_dir:
-      os.rmdir(path)
-    else:
-      os.remove(path)
-  except BaseException as exception:
-    print(exception)
+    try:
+        if is_dir:
+            os.rmdir(path)
+        else:
+            os.remove(path)
+    except BaseException as exception:
+        print(exception)
 
 
 def move(event):
-  """
-  Moves (renames) a directory or file. If the slave is not in sync,
-  this may cause an exception which is caught.
-  """
-  old = event['data']['old']
-  path = event['data']['path']
+    """
+    Moves (renames) a directory or file. If the slave is not in sync,
+    this may cause an exception which is caught.
+    """
+    old = event['data']['old']
+    path = event['data']['path']
 
-  try:
-    os.replace(old, path)
-  except BaseException as exception:
-    print(exception)
+    try:
+        os.replace(old, path)
+    except BaseException as exception:
+        print(exception)
 
 
 def onopen(service):
-  """
-  SPARKL callback sets implementation dict.
-  """
-  print("Slave listener started in {cwd}".format(
-    cwd=os.getcwd()))
+    """
+    SPARKL callback sets implementation dict.
+    """
+    print("Slave listener started in {cwd}".format(
+        cwd=os.getcwd()))
 
-  service.impl = {
-    'Mix/Slave/Put':    put,
-    'Mix/Slave/Delete': delete,
-    'Mix/Slave/Move':   move}
+    service.impl = {
+        'Mix/Slave/Put':    put,
+        'Mix/Slave/Delete': delete,
+        'Mix/Slave/Move':   move}
 
 
-def onclose(service):
-  """
-  SPARKL callback
-  """
-  print("Slave listener stopped")
+def onclose(_service):
+    """
+    SPARKL callback
+    """
+    print("Slave listener stopped")
